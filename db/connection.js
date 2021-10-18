4eac142722e0cc55a99d54946350913ff7cc8b82
Adam <ats92@cox.net>
2021-10-18 09:42:50 -0700

    added voter routes

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '!21Herbert21!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
+
+
+module.exports = db;
